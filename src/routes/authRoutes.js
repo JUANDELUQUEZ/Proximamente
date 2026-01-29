82f19554c67da9e3641423e7e07b828c65ee8409
JUANDELUQUEZ <juandeluquez1123@gmial.com>
2026-01-28 22:13:08 -0600

    back-end

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-// Definimos la ruta POST para el login
-// La URL final será: http://localhost:3000/api/auth/login
+// Ruta: POST /api/auth/login
 router.post("/login", authController.login);
 
 module.exports = router;
