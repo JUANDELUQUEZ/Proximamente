5dd737a48a3d0d89d22ef6436f4431cc65fc1c30
JUANDELUQUEZ <juandeluquez1123@gmial.com>
2026-01-29 21:06:14 -0600

    Corrección de rutas estáticas para despliegue

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path"); // <--- 1. Importamos esto
 const connectDB = require("./src/config/database");
 
 // Rutas importadas
@@ -12,12 +13,21 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors()); // Permite que el frontend de tu amigo se conecte
-app.use(express.json()); // Permite leer JSON en las peticiones
-app.use(express.static("src/public")); // Servir archivos estáticos (frontend)
+app.use(cors());
+app.use(express.json());
+
+// --- CORRECCIÓN IMPORTANTE AQUÍ ---
+// Usamos path.join para que funcione en la nube (Linux) y en local
+app.use(express.static(path.join(__dirname, "src/public")));
 
 // Definir rutas de la API
 app.use("/api/auth", authRoutes);
 app.use("/api/requests", requestRoutes);
+
+// --- AGREGAMOS ESTO POR SEGURIDAD ---
+// Si alguien entra a la raíz '/', le enviamos el index.html a la fuerza
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/public", "index.html"));
+});
 
 module.exports = app;
